5f436bf3dd5b5af82929f39d3761d37eb7f44392
Jorge L Rios <rios.a.jorge.luis@gmail.com>
2024-06-30 08:52:02 -0600

    knaub and company first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Initialize Scroll Reveal
+ScrollReveal().reveal('.content', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px',
+    reset: true
+  });
+  
+  // You can add more ScrollReveal animations for different elements
+  ScrollReveal().reveal('.footer-section h5', { duration: 1000, origin: 'left', distance: '50px' });
+  ScrollReveal().reveal('.footer-section p', { duration: 1500, origin: 'right', distance: '50px' });
+  ScrollReveal().reveal('.footer-section ul li', { duration: 2000, origin: 'bottom', distance: '50px' });
+  
